d3c3fe84cb5a5b8904c1bb9b6103ef73900946b7
avsalazarm <salazar.avm@gmail.com>
2022-10-14 21:08:02 -0400

    files updated

--- blog-service.js
+++ blog-service.js
@@ -55,6 +55,51 @@ module.exports.getPublishedPosts = function() {
     });
 }
 
+module.exports.getPostsbyCategories = function(category){
+    return new Promise(function(resolve,reject){
+        var foundCategory = null;
+        for(let i = 0; i < posts.length && !foundCategory; i++){
+            if(posts[i].category==category){
+                foundCategory = posts[i];
+            }
+        }
+        if(!foundCategory){
+            reject("Category does not exist - No data to be shown");
+        }
+        resolve(foundCategory);
+    })
+}
+
+module.exports.getPostsByMinDate = function(minDateStr){
+    return new Promise(function(resolve,reject){
+        var foundDate = null;
+        for(let i = 0; i < posts.length && !foundDate; i++){
+            if(newDate(posts[i].postDate) >= newDate(minDateStr)){
+                foundDate = posts[i];
+            }
+        }
+        if(!foundDate){
+            reject("Date does not exist - No data to be shown");
+        }
+        resolve(foundDate);
+    })
+}
+
+module.exports.getPostsbyId = function(id){
+    return new Promise(function(resolve,reject){
+        var foundPost = null;
+        for(let i = 0; i < posts.length && !foundPost; i++){
+            if(posts[i].id==id){
+                foundPost = posts[i];
+            }
+        }
+        if(!foundPost){
+            reject("ID does not exist - No data to be shown");
+        }
+        resolve(foundPost);
+    })
+}
+
 module.exports.getCategories = function() {
     return new Promise((resolve, reject)=>{
         if(categories.length == 0){
@@ -64,3 +109,12 @@ module.exports.getCategories = function() {
         }
     });
 }
+
+module.exports.addPost = function(postData){
+    return new Promise(function(resolve,reject){
+        postData.id = posts.length + 1; //automatic creation of an id
+        postData.published = (postData.published)? true: false; //if the box is checked it's set to true
+        posts.push(postData); //will add the new post to the array
+        resolve();
+    });
+};
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
@@ -8,10 +8,22 @@
 *  Online (Cyclic) Link: https://dull-lime-sea-lion-kilt.cyclic.app
 *
 ********************************************************************************/ 
+const multer = require('multer');
+const cloudinary = require('cloudinary');
+const streamifier = require('streamifier');
+const upload = multer(); // no { storage: storage } 
+
+cloudinary.config({
+  cloud_name: 'Cloud Name',
+  api_key: 'API Key',
+  api_secret: 'API Secret',
+  secure: true
+});
 
 var express = require ("express");
 const path = require("path");
-const data = require('./blog-service.js')
+const data = require('./blog-service.js');
+const e = require('express');
 var app = express();
 
 var HTTP_PORT = process.env.PORT || 8080;
@@ -35,6 +47,74 @@ app.get("/posts", function(req, res){
     res.json(data);
   });
 });
+
+//new function so the user can access the form and add a new post
+app.get("/posts/add", function(req, res){
+  res.sendFile(path.join(__dirname, "/views/addPost.html"));
+});
+
+//controls what happens to the data after the user inputs something
+app.post("/posts/add",upload.single("featureImage"),(req, res)=>{
+  if(req.file){
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream(
+            (error, result) => {
+              if (result) {
+                  resolve(result);
+              } else {
+                  reject(error);
+              }
+            }
+        );
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+    });
+};
+
+async function upload(req) {
+    let result = await streamUpload(req);
+    console.log(result);
+    return result;
+}
+
+upload(req).then((uploaded)=>{
+    processPost(uploaded.url);
+  });
+}else{
+  processPost("");
+}
+
+function processPost(imageUrl){
+    req.body.featureImage = imageUrl;
+
+    data.addPost(req.body).then(post=>{
+      res.redirect("/posts");
+    }).catch(err=>{
+      res.status(500).send(err);
+    })
+  }
+});
+ 
+app.get("/posts?category", function(req, res){
+  data.getPostsbyCategories(category).then((data)=>{
+    res.json(data);
+  });
+});
+
+app.get("/posts?minDate", function(req, res){
+  data.getPostsByMinDate(minDateStr).then((data)=>{
+    res.json(data);
+  });
+});
+
+
+app.get("/posts/id", function(req, res){
+  data.getPostById(id).then((data)=>{
+    res.json(data);
+  });
+});
+
+
 
 app.get("/blog", function(req, res){
   data.getPublishedPosts().then((data)=>{
